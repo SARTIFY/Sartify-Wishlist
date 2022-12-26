870af8e24d161212a87ddb8250d5a96c59b4a1ef
innocent-charles <innocentcharles217@gmail.com>
2022-12-26 14:48:21 +0300

    remove the shadow in the image

--- pages/index.js
+++ pages/index.js
@@ -116,7 +116,7 @@ export default function Home() {
           </div>
           </div>
           <div className="relative">
-            <div className="absolute inset-0">
+            {/* <div className="absolute inset-0">
               <svg className="blur-3xl filter opacity-70" style={{filter:"blur(64px)"}} width="444" height="536" viewBox="0 0 444 536" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M225.919 112.719C343.98 64.6648 389.388 -70.487 437.442 47.574C485.496 165.635 253.266 481.381 135.205 529.435C17.1445 577.488 57.9596 339.654 9.9057 221.593C-38.1482 103.532 107.858 160.773 225.919 112.719Z" fill="url(#c)" />
                   <defs>
@@ -126,7 +126,7 @@ export default function Home() {
                     </linearGradient>
                   </defs>
               </svg>
-            </div>
+            </div> */}
             <div className="absolute inset-0">
               <img className="object-cover w-full h-full opacity-90" src="https://landingfoliocom.imgix.net/store/collection/dusk/images/noise.png" alt="" />
             </div>
