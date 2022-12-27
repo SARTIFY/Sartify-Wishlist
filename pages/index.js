902211587dfca1393cbb0161676e09dd835876e8
innocent-charles <innocentcharles217@gmail.com>
2022-12-27 17:19:53 +0300

    change codebase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import NavBar from '../components/NavBar';
+import Image from 'next/image';
+import Link from 'next/link';
 export default function Home() {
   return (
   <div className="">
+    {/* <NavBar /> */}
     <header className="py-4 bg-black sm:py-6" x-data="{expanded: false}" style={{marginTop:"-60px"}}>
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
@@ -10,7 +14,7 @@ export default function Home() {
             </a>
           </div>
           <div className="flex md:hidden">
-            <button type="button" className="text-white" onclick="expanded = !expanded" aria-expanded="expanded">
+            {/* <button type="button" className="text-white" onclick="expanded = !expanded" aria-expanded="expanded">
               <span x-show="!expanded" aria-hidden="true">
                 <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 6h16M4 12h16M4 18h16" />
@@ -21,18 +25,17 @@ export default function Home() {
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                  </svg>
               </span>
-            </button>
+            </button> */}
           </div>
-          <nav className="hidden ml-10 mr-auto space-x-10 lg:ml-20 lg:space-x-12 md:flex md:items-center md:justify-start">
+          <nav className="hidden ml-10 space-x-10 lg:ml-20 lg:space-x-12 md:flex md:items-center md:justify-start" style={{marginRight:"25px"}}>
             <a href="#" title="" class="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Products </a>
             <a href="#" title="" class="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Team </a>
             <a href="#" title="" class="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Contact </a>
-            {/* <a href="#" title="" class="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white"> Support </a> */}
           </nav>
-          <div className="relative hidden md:items-center md:justify-center md:inline-flex group">
+          {/* <div className="relative  md:items-center md:justify-center md:inline-flex group">
             <div className="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
             <a href="#" title="" className="relative inline-flex items-center justify-center px-6 py-2 text-base font-normal text-white bg-black border border-transparent rounded-full" role="button"> Join Wishlist </a>
-          </div>
+          </div> */}
         </div>
         {/* <nav x-show="expanded" x-collapse>
           <div className="flex flex-col pt-8 pb-4 space-y-6">
@@ -59,7 +62,13 @@ export default function Home() {
           <p className="mt-4 max-w-3xl space-y-6 text-gray-400 sm:mt-8">HR administration may be tough, but it’s no rocket science. We help great companies in Africa unlock their workforce potential, effortlessly save 200hrs for efficient in talent acquisition, interview, retention, payrolling, staffing, and organizational development.</p>
           
           <div className="relative mt-8 rounded-full sm:mt-12">
-            <p className="mt-4 max-w-3xl space-y-6 text-gray-400 sm:mt-8 hover:text-blue-600" style={{fontSize:"10px"}}>WISHLISTED BY 50+ USERS</p>
+            <p className="mt-4 max-w-3xl space-y-6 text-gray-400 sm:mt-8 hover:text-blue-600" style={{fontSize:"10px",marginBottom:"10px"}}>WISHLISTED BY 50+ USERS</p> 
+
+            <div className="relative md:items-center md:justify-center md:inline-flex group" style={{marginLeft:"100px"}}>
+            <div className="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
+            <a href="#" title="" className="relative inline-flex items-center justify-center px-6 py-2 text-base font-normal text-white bg-black border border-transparent rounded-full" role="button"> Get Eearly Access - it's free </a>
+          </div>
+          
           </div>
 
           <div className="mt-8 sm:mt-12">
