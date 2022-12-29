0daa6282e0bee2882e855574c587ccb3088b8758
innocent-charles <innocentcharles217@gmail.com>
2022-12-29 14:10:45 +0300

    final last code

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={{backgroundColor:"black"}}>
       <Head>
       <script async src="https://tally.so/widgets/embed.js"></script>
       </Head>
