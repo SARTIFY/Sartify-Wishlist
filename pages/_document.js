b25accbbdc59689336757bc8c486d6ce7072784c
innocent-charles <innocentcharles217@gmail.com>
2022-12-30 10:22:38 +0300

    optimize the SEO

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,11 @@ export default function Document() {
   return (
     <Html lang="en" style={{backgroundColor:"black"}}>
       <Head>
-      <script async src="https://tally.so/widgets/embed.js"></script>
+        <meta charSet='utf-8'/>
+        <meta name="viewport" content="width=device-width"/>
+        <title>Sartify : The smart HR software for Africa's Businesses</title>
+        <meta name="description" content="Effortlessly save 200hrs for efficient and costless talent acquisition, interview, retention, payrolling, staffing, time-off, employee data, organizational development, and HR workflows in one place."/>
+        <script async src="https://tally.so/widgets/embed.js"></script>
       </Head>
       <body>
         <Main />
